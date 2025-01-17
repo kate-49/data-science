7cbd943bde8d420de3fb000041be36ae22b249e0
Kate Smith <kate.smith@digital.justice.gov.uk>
2025-01-17 17:34:55 +0000

    finished roux challenge

--- learn_numpy/Roux/roux.py
+++ learn_numpy/Roux/roux.py
@@ -1,3 +1,28 @@
+import numpy as np
+
 class Roux:
+    def __init__(self, number):
+        self.number = number
+
     def run(self):
-        return "3"
+        if self.number % 2 == 0:
+            length = self.number / 2
+        else:
+            length = (self.number + 1) / 2
+        array = np.arange(start=int(length), stop=0, step=-1)
+        array = (np.power(array, 2))
+
+        if self.number % 2 == 0:
+            array = np.insert(array, 0, 0, axis=0)
+            index = 2
+        else:
+            index = 1
+
+        for x in array:
+            current_length = array.size
+            if index < current_length:
+                array = np.insert(array, index, 0, axis=0)
+                index += 2
+
+        print(array)
+        return np.array(array)
--- learn_numpy/Roux/test_roux.py
+++ learn_numpy/Roux/test_roux.py
@@ -1,8 +1,13 @@
 import unittest
+import roux as r
+import numpy as np
 
 class TestRout(unittest.TestCase):
     def test_run(self):
-        self.assertEqual(self.run(), "88" , "Should be 88")
+
+        self.assertTrue(np.array_equal(r.Roux(5).run(),np.array([9, 0, 4, 0, 1])))
+        self.assertTrue(np.array_equal(r.Roux(8).run(),np.array([0, 16, 0, 9, 0, 4, 0, 1])))
+        self.assertTrue(np.array_equal(r.Roux(12).run(),np.array([0, 36, 0, 25,  0, 16, 0, 9, 0, 4, 0, 1])))
 
 if __name__ == "__main__":
     unittest.main()
